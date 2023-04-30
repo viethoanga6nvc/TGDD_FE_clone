ebf01b6335736a5c4b5c93648e10a0b7f9517a26
viethoanga6nvc <viethoang.a6nvc@gmail.com>
2023-04-30 21:39:10 +0700

    lab2

--- TGDD_clone/Home/HomePage.js
+++ TGDD_clone/Home/HomePage.js
@@ -1,1 +1,11 @@
 console.log("Home Page.");
+$(function () {
+    loadComponent();
+});
+
+function loadComponent() {
+    $(".MenuSection").load("./Menu.html");
+    $(".BannerSection").load("./Banner.html");
+    $(".ProductSection").load("./Products.html");
+    $(".FooterSection").load("./Footer.html");
+}
