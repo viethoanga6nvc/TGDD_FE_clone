6cc07ecde946f13a7739e34bb44c84fef668916e
viethoanga6nvc <viethoang.a6nvc@gmail.com>
2023-05-01 14:01:19 +0700

    lad6

--- TGDD_clone/Admin/AdminPage.js
+++ TGDD_clone/Admin/AdminPage.js
@@ -5,5 +5,21 @@ $(function () {
 function loadComponentAdmin() {
     $(".MenuSection").load("./MenuAdmin.html");
     $(".SideBarSection").load("./SideBarAdmin.html");
-    $(".ProductAdminSection").load("./ContentProduct.html");
+    // $(".ProductAdminSection").load("./ContentProduct.html");
+}
+
+function handleShowManufacturer() {
+    $(".ProductAdminSection").load("./ContentManufacturer.html", "data", function (response, status, request) { });
+}
+
+function handleShowCategory() {
+    $(".ProductAdminSection").load("./ContentCategory.html", "data", function (response, status, request) { });
+}
+
+function handleShowProduct() {
+    $(".ProductAdminSection").load("./ContentProduct.html", "data", function (response, status, request) { });
+}
+
+function handleShowAccount() {
+    $(".ProductAdminSection").load("./ContentAccount.html", "data", function (response, status, request) { });
 }
