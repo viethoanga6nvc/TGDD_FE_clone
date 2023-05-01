310c1df4194d94470327955412005b357a37b0eb
viethoanga6nvc <viethoang.a6nvc@gmail.com>
2023-05-01 15:03:39 +0700

    lab8

--- TGDD_clone/Admin/AdminPage.js
+++ TGDD_clone/Admin/AdminPage.js
@@ -17,7 +17,9 @@ function handleShowCategory() {
 }
 
 function handleShowProduct() {
-    $(".ProductAdminSection").load("./ContentProduct.html", "data", function (response, status, request) { });
+    $(".ProductAdminSection").load("./ContentProduct.html", "data", function (response, status, request) {
+        featchListProduct();
+    });
 }
 
 function handleShowAccount() {
@@ -26,4 +28,27 @@ function handleShowAccount() {
 
 function handleCreateNewProduct() {
     alert("Create New!!")
+}
+
+function featchListProduct() {
+    $("#tbProductAdmin").empty();
+
+    for (let index = 0; index < 6; index++) {
+        $("#tbProductAdmin").append(`
+        <tr>
+            <td>1</td>
+            <td>Samsung Galaxy S22 Ultra 5G</td>
+            <td>30.998.000₫</td>
+            <td>6.9 inches, Chip MediaTek Helio G85 (12nm) mạnh mẽ, ram 4G, Pin
+                7000 mAh</td>
+            <td>ProductDetail1</td>
+            <td>5</td>
+            <td>image1.jpg</td>
+            <td>SAMSUNG</td>
+            <td>Điện thoại</td>
+            <td><button class="btn btn-waring" type="button">Edit</button></td>
+            <td><button class="btn btn-danger" type="button">Delete</button></td>
+        </tr>
+        `);
+    }
 }
