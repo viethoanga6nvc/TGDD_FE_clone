7d0f52f342eceb6c533a46a669ecd6b99a695a87
viethoanga6nvc <viethoang.a6nvc@gmail.com>
2023-05-03 21:46:07 +0700

    lab10

--- TGDD_clone/Home/HomePage.js
+++ TGDD_clone/Home/HomePage.js
@@ -1,4 +1,4 @@
-console.log("Home Page.");
+var listProduct = [];
 $(function () {
     loadComponent();
 
@@ -15,26 +15,33 @@ function loadComponent() {
 }
 
 function featchListProduct() {
-    for (let index = 0; index < 8; index++) {
+    listProduct = [];
+
+    if (localStorage && localStorage.getItem("listProduct")) {
+        var listProductLocalStorage = JSON.parse(localStorage.getItem("listProduct"));
+        listProduct = listProductLocalStorage;
+    }
+
+    listProduct.forEach(element => {
         $(".ProductList").append(`
         <!-- SP 1 -->
-        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3" style="text-align: left;">
+        <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3" style="text-align: left; width: 300px; height: 500px;">
             <!-- Ảnh -->
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                    <img src="../Asset/Product/ImgMobile1.png" alt="" style="width: 160px; height: 190px;" />
+                    <img src="../Asset/Product/${element.imageName}" alt="" style="width: 160px; height: 190px;" />
                 </div>
             </div>
             <!-- Tên sản phẩm -->
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                    <h3 style="font-weight: bold;">Reno6 Z 5G</h3>
+                    <h3 style="font-weight: bold;">${element.name}</h3>
                 </div>
             </div>
             <!-- Hãng sản phẩm -->
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                    <h4>Hãng sản phẩm: OPPO</h4>
+                    <h4>Hãng sản phẩm:${element.manufacturer}</h4>
                 </div>
             </div>
             <!-- Đánh giá -->
@@ -42,28 +49,14 @@ function featchListProduct() {
                 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                     <ul class="rating"
                         style="list-style: none; display: flex; font-size: 20px; padding-inline-start: 0px;">
-                        <li>
-                            <i class="fa fa-star selected" style="color: orange;"></i>
-                        </li>
-                        <li>
-                            <i class="fa fa-star selected" style="color: orange;"></i>
-                        </li>
-                        <li>
-                            <i class="fa fa-star selected" style="color: orange;"></i>
-                        </li>
-                        <li>
-                            <i class="fa fa-star"></i>
-                        </li>
-                        <li>
-                            <i class="fa fa-star"></i>
-                        </li>
+                        ${showStarRating(element.star)}
                     </ul>
                 </div>
             </div>
             <!-- Thêm vào giỏ hàng -->
             <div class="row">
                 <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-                    <h4>9.490.000₫</h4>
+                    <h4>${element.price}</h4>
                 </div>
 
                 <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
@@ -76,5 +69,25 @@ function featchListProduct() {
         </div>
         <!-- End SP1 -->
         `);
+    });
+}
+
+function showStarRating(ratingStar) {
+    let starRating = "";
+
+    for (let index = 1; index <= ratingStar; index++) {
+        starRating += `
+        <li>
+            <i class="fa fa-star selected" style="color: orange;"></i>
+        </li>`
     }
+
+    for (let index = 1; index <= 5 - ratingStar; index++) {
+        starRating += `
+        <li>
+            <i class="fa fa-star"></i>
+        </li>`
+    }
+
+    return starRating;
 }
