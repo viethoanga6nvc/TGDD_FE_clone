addacc4de6031d1d143ab4f1da973a07f10a97a1
viethoanga6nvc <viethoang.a6nvc@gmail.com>
2023-12-01 13:06:12 +0700

    finish

--- TGDD_clone/Admin/AdminPage.js
+++ TGDD_clone/Admin/AdminPage.js
@@ -1,75 +1,130 @@
 var listProduct = [];
 var idProductUpdate = "";
 $(function () {
-    loadComponentAdmin();
+  loadComponentAdmin();
 });
 
 function loadComponentAdmin() {
-    $(".MenuSection").load("./MenuAdmin.html");
-    $(".SideBarSection").load("./SideBarAdmin.html");
-    // $(".ProductAdminSection").load("./ContentProduct.html");
+  $(".MenuSection").load("./MenuAdmin.html");
+  $(".SideBarSection").load("./SideBarAdmin.html");
+  // $(".ProductAdminSection").load("./ContentProduct.html");
 }
 
 function handleShowManufacturer() {
-    $(".ProductAdminSection").load("./ContentManufacturer.html", "data", function (response, status, request) { });
+  $(".ProductAdminSection").load(
+    "./ContentManufacturer.html",
+    "data",
+    function (response, status, request) {}
+  );
 }
 
 function handleShowCategory() {
-    $(".ProductAdminSection").load("./ContentCategory.html", "data", function (response, status, request) { });
+  $(".ProductAdminSection").load(
+    "./ContentCategory.html",
+    "data",
+    function (response, status, request) {}
+  );
 }
 
 function handleShowProduct() {
-    $(".ProductAdminSection").load("./ContentProduct.html", "data", function (response, status, request) {
-        featchListProduct();
-    });
+  $(".ProductAdminSection").load(
+    "./ContentProduct.html",
+    "data",
+    function (response, status, request) {
+      featchListProduct();
+    }
+  );
 }
 
 function handleShowAccount() {
-    $(".ProductAdminSection").load("./ContentAccount.html", "data", function (response, status, request) { });
+  $(".ProductAdminSection").load(
+    "./ContentAccount.html",
+    "data",
+    function (response, status, request) {}
+  );
 }
 
 function handleCreateNewProduct() {
-    var v_Id = $("#Id").val();
-    var v_Name = $("#Name").val();
-    var v_Price = $("#Price").val();
-    var v_Info = $("#Info").val();
-    var v_Detail = $("#Detail").val();
-    var v_Star = $("#Star").val();
-    var v_Image = getImageName($("#Image").val());
-    var v_Manufacturer = $("#Manufacturers").val();
-    var v_Category = $("#Category").val();
+  var v_Id = $("#Id").val();
+  var v_Name = $("#Name").val();
+  var v_Price = $("#Price").val();
+  var v_Info = $("#Info").val();
+  var v_Detail = $("#Detail").val();
+  var v_Star = $("#Star").val();
+  var v_Image = getImageName($("#Image").val());
+  var v_Manufacturer = $("#Manufacturers").val();
+  var v_Category = $("#Category").val();
 
-    var ProductNew = {
-        id: v_Id,
-        name: v_Name,
-        price: v_Price,
-        info: v_Info,
-        detail: v_Detail,
-        star: v_Star,
-        imageName: v_Image,
-        manufacturer: v_Manufacturer,
-        category: v_Category,
-    };
+  var ProductNew = {
+    id: v_Id,
+    name: v_Name,
+    price: v_Price,
+    info: v_Info,
+    detail: v_Detail,
+    star: v_Star,
+    imageName: v_Image,
+    manufacturer: v_Manufacturer,
+    category: v_Category,
+  };
 
-    listProduct.push(ProductNew);
-    localStorage.setItem("listProduct", JSON.stringify(listProduct));
-    handleResetForm();
-    featchListProduct();
-    alert("Thêm mới thành công!")
+  listProduct.push(ProductNew);
+  localStorage.setItem("listProduct", JSON.stringify(listProduct));
+  handleResetForm();
+
+  $("#myModal").modal("hide");
+
+  featchListProduct();
+  alert("Thêm mới thành công!");
 }
 
 function featchListProduct() {
-    listProduct = [];
+  listProduct = [];
 
-    if (localStorage && localStorage.getItem("listProduct")) {
-        var listProductLocalStorage = JSON.parse(localStorage.getItem("listProduct"));
-        listProduct = listProductLocalStorage;
-    }
+  if (localStorage && localStorage.getItem("listProduct")) {
+    var listProductLocalStorage = JSON.parse(
+      localStorage.getItem("listProduct")
+    );
+    listProduct = listProductLocalStorage;
+  }
+  {
+    // $.ajax({
+    //     type: "GET",
+    //     url: "https://645a3f7295624ceb21fc1fed.mockapi.io/products",
+    //     // data: "data",
+    //     dataType: "json",
+    //     success: function (response, status) {
+    //         if (status === "success") {
+    //             listProduct = response;
+    //             $("#tbProductAdmin").empty();
+    //             listProduct.forEach(element => {
+    //                 $("#tbProductAdmin").append(`
+    //                 <tr>
+    //                     <td>${element.id}</td>
+    //                     <td>${element.name}</td>
+    //                     <td>${element.price}</td>
+    //                     <td>${element.info}</td>
+    //                     <td>${element.detail}</td>
+    //                     <td>${element.star}</td>
+    //                     <td>${element.imageName}</td>
+    //                     <td>${element.manufacturer}</td>
+    //                     <td>${element.category}</td>
+    //                     <td><button class="btn btn-waring" type="button" onclick="handleEdit(${element.id})">Edit</button></td>
+    //                     <td><button class="btn btn-danger" type="button" onclick="handleDelete(${element.id})">Delete</button></td>
+    //                 </tr>
+    //                 `);
+    //             });
+    //         } else {
+    //             console.log("Error when loading data!");
+    //             return;
+    //         }
+    //     }
+    // });
+  }
 
-    $("#tbProductAdmin").empty();
+  $("#tbProductAdmin").empty();
 
-    listProduct.forEach(element => {
-        $("#tbProductAdmin").append(`
+  listProduct.forEach((element) => {
+    $("#tbProductAdmin").append(`
         <tr>
             <td>${element.id}</td>
             <td>${element.name}</td>
@@ -84,99 +139,100 @@ function featchListProduct() {
             <td><button class="btn btn-danger" type="button" onclick="handleDelete(${element.id})">Delete</button></td>
         </tr>
         `);
-    });
+  });
 }
 
 function handleResetForm() {
-    $("#Id").val("");
-    $("#Name").val("");
-    $("#Price").val("");
-    $("#Info").val("");
-    $("#Detail").val("");
-    $("#Star").val("");
-    $("#Image").val("");
-    $("#Manufacturers").val("");
-    $("#Category").val("");
+  $("#Id").val("");
+  $("#Name").val("");
+  $("#Price").val("");
+  $("#Info").val("");
+  $("#Detail").val("");
+  $("#Star").val("");
+  $("#Image").val("");
+  $("#Manufacturers").val("");
+  $("#Category").val("");
 }
 
 function getImageName(pathImage) {
-    var itemArray = pathImage.split("\\");
-    var imageName = itemArray[itemArray.length - 1];
+  var itemArray = pathImage.split("\\");
+  var imageName = itemArray[itemArray.length - 1];
 
-
-    return imageName;
+  return imageName;
 }
 
 function handleDelete(idDelete) {
-    var confirmDelete = confirm("Bạn có chắc chắn muốn xóa sản phẩm này không?");
-    if (confirmDelete) {
-        var indexProductDelete = listProduct.findIndex((product) => product.id == idDelete);
-        if (indexProductDelete !== -1) {
-            listProduct.splice(indexProductDelete, 1);
-            localStorage.setItem("listProduct", JSON.stringify(listProduct));
-            featchListProduct();
-            alert("Xóa thành công!")
-        } else {
-            alert("Không thể xóa sản phẩm!")
-        }
+  var confirmDelete = confirm("Bạn có chắc chắn muốn xóa sản phẩm này không?");
+  if (confirmDelete) {
+    var indexProductDelete = listProduct.findIndex(
+      (product) => product.id == idDelete
+    );
+    if (indexProductDelete !== -1) {
+      listProduct.splice(indexProductDelete, 1);
+      localStorage.setItem("listProduct", JSON.stringify(listProduct));
+      featchListProduct();
+      alert("Xóa thành công!");
+    } else {
+      alert("Không thể xóa sản phẩm!");
     }
+  }
 }
 
 function handleEdit(idEdit) {
-    idProductUpdate = idEdit;
-    var index = listProduct.findIndex((product) => product.id == idProductUpdate);
+  idProductUpdate = idEdit;
+  var index = listProduct.findIndex((product) => product.id == idProductUpdate);
 
-    $("#IdUpdate").val(listProduct[index].id);
-    $("#NameUpdate").val(listProduct[index].name);
-    $("#PriceUpdate").val(listProduct[index].price);
-    $("#InfoUpdate").val(listProduct[index].info);
-    $("#DetailUpdate").val(listProduct[index].detail);
-    $("#StarUpdate").val(listProduct[index].star);
-    $("#ManufacturersUpdate").val(listProduct[index].manufacturer);
-    $("#CategoryUpdate").val(listProduct[index].category);
+  $("#IdUpdate").val(listProduct[index].id);
+  $("#NameUpdate").val(listProduct[index].name);
+  $("#PriceUpdate").val(listProduct[index].price);
+  $("#InfoUpdate").val(listProduct[index].info);
+  $("#DetailUpdate").val(listProduct[index].detail);
+  $("#StarUpdate").val(listProduct[index].star);
+  $("#ManufacturersUpdate").val(listProduct[index].manufacturer);
+  $("#CategoryUpdate").val(listProduct[index].category);
 
-    $("#myModalUpdateProduct").modal("show");
+  $("#myModalUpdateProduct").modal("show");
 }
 
 function handleResetProduct() {
-    $("#NameUpdate").val("");
-    $("#PriceUpdate").val("");
-    $("#InfoUpdate").val("");
-    $("#DetailUpdate").val("");
-    $("#StarUpdate").val("");
-    $("#ImageUpdate").val("");
-    $("#ManufacturersUpdate").val("");
-    $("#CategoryUpdate").val("");
+  $("#NameUpdate").val("");
+  $("#PriceUpdate").val("");
+  $("#InfoUpdate").val("");
+  $("#DetailUpdate").val("");
+  $("#StarUpdate").val("");
+  $("#ImageUpdate").val("");
+  $("#ManufacturersUpdate").val("");
+  $("#CategoryUpdate").val("");
 }
 
 function handleUpdateProduct() {
-    var index = listProduct.findIndex((product) => product.id == idProductUpdate);
+  var index = listProduct.findIndex((product) => product.id == idProductUpdate);
 
-    var v_Name = $("#NameUpdate").val();
-    var v_Price = $("#PriceUpdate").val();
-    var v_Info = $("#InfoUpdate").val();
-    var v_Detail = $("#DetailUpdate").val();
-    var v_Star = $("#StarUpdate").val();
-    var v_Image = getImageName($("#ImageUpdate").val());
-    var v_Manufacturer = $("#ManufacturersUpdate").val();
-    var v_Category = $("#CategoryUpdate").val();
+  var v_Name = $("#NameUpdate").val();
+  var v_Price = $("#PriceUpdate").val();
+  var v_Info = $("#InfoUpdate").val();
+  var v_Detail = $("#DetailUpdate").val();
+  var v_Star = $("#StarUpdate").val();
+  var v_Image = getImageName($("#ImageUpdate").val());
+  var v_Manufacturer = $("#ManufacturersUpdate").val();
+  var v_Category = $("#CategoryUpdate").val();
 
-    listProduct[index].name = v_Name;
-    listProduct[index].price = v_Price;
-    listProduct[index].info = v_Info;
-    listProduct[index].detail = v_Detail;
-    listProduct[index].star = v_Star;
-    if (v_Image !== null && v_Image !== "") {
-        listProduct[index].imageName = v_Image;
-    }
-    listProduct[index].manufacturer = v_Manufacturer;
-    listProduct[index].category = v_Category;
+  listProduct[index].name = v_Name;
+  listProduct[index].price = v_Price;
+  listProduct[index].info = v_Info;
+  listProduct[index].detail = v_Detail;
+  listProduct[index].star = v_Star;
+  if (v_Image !== null && v_Image !== "") {
+    listProduct[index].imageName = v_Image;
+  }
+  listProduct[index].manufacturer = v_Manufacturer;
+  listProduct[index].category = v_Category;
 
-    localStorage.setItem("listProduct", JSON.stringify(listProduct));
+  localStorage.setItem("listProduct", JSON.stringify(listProduct));
 
-    handleResetForm();
+  handleResetForm();
 
-    $("#myModalUpdateProduct").modal("hide");
+  $("#myModalUpdateProduct").modal("hide");
 
-    featchListProduct();
+  featchListProduct();
 }
